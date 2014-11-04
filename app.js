4e2b06d266d5110b4936170df7c15a90e522be61
Jon Gamble <jon@centerline.net>
2014-11-04 11:17:15 -0500

    Starting to add in node components

---
+++ app.js
@@ -0,0 +1,35 @@
+// set variables for environment
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 80;
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var path = require('path');
+
+
+var bodyParser = require('body-parser')
+
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+
+// views as directory for all template files
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use( bodyParser.json() );
+app.use(express.static('public'));
+
+// set routes
+    app.get('/', function(req, res) {
+      res.render('index');
+    });
+
+
+require('./app/recipes.js')(app, mongoose);
+require('./app/profile.js')(app, mongoose);
+
+app.listen(port);
+console.log('server is running');
